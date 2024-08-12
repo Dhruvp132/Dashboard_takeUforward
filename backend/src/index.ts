e903bd226bf6d817ec9a0a9d2b0fadf7d5acd90c
Dhruvp132 <dhruvpatel1321@gmail.com>
2024-08-12 22:44:03 +0530

    five

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,26 @@
-import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { cors } from 'hono/cors';
-import { dataRouter } from './routes/data';
+import { handle } from '@hono/node-server/vercel'
+import { cors } from 'hono/cors'
+import { dataRouter } from './routes/data'
 
-const app = new Hono()
+const app = new Hono().basePath('/api')
 
+// Enable CORS for all routes
 app.use('/*', cors())
 
-app.route("/api/v1/", dataRouter); 
+// Use the data router for versioned API routes
+app.route('/api/v1/', dataRouter)
 
+// Define additional routes
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
-
-serve({
-  fetch: app.fetch,
-  port
+app.get('/hello', (c) => {
+  return c.json({
+    message: 'Hello from Hono!',
+  })
 })
+
+// Export the handler for Vercel
+export default handle(app)
